14efcc583c5dcad1ca113ab4a0566d675d9990fe
Lex <simplgod@gmail.com>
2024-03-31 03:53:47 +0300

    tabstop 4

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,7 @@
 -- Make line numbers default
 vim.opt.number = true
 vim.opt.rnu = true
+vim.opt.tabstop = 4
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.opt.relativenumber = true
